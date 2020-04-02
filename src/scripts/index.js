a228a8f5e39ae145a6224985815ebef3c7dd5cd1
KalaitanovRoman <kalaitanovr@gmail.com>
2020-04-02 17:54:47 +0300

    add footer and map block

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,28 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+(function main() {
+        const blackout = document.querySelector('.blackout');
+        const arrow = document.querySelector('.arrow-down');
+
+        arrow.addEventListener('click', (e) => {
+            console.log('!!!', blackout);
+            blackout.style.transform = 'scale(0)';
+        });
+    }
+)();
+
+const init = () => {
+    const myMap = new ymaps.Map("map", {
+        center: [60.005090, 30.245701],
+        zoom: 14.5
+    });
+
+    myMap.behaviors.disable('scrollZoom');
+
+    myMap.geoObjects
+        .add(new ymaps.Placemark([60.005090, 30.245701], {
+            balloonContent: 'Здароова <strong>петух!</strong>'
+        }));
+};
+
+ymaps.ready(init);
