0c5b82974cd440280d956a71a0a9af95889b124a
KalaitanovRoman <kalaitanovr@gmail.com>
2020-04-07 14:31:25 +0300

    Added subscribe and price blocks

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,20 +1,24 @@
 import '../styles/index.scss';
 
-document.getElementById("form-submit").addEventListener("click", (e) => {
-    e.preventDefault();
+const buttons = document.querySelectorAll('button');
+const body = document.querySelector('body');
 
-    console.log('Form submitted');
+//TODO: delete this code after create actions on buttons
+body.addEventListener("click", (e) => {
+    for (let item of buttons) {
+        e.preventDefault();
+        console.log('Form submitted');
+    }
 });
 
 (function main() {
-        const blackout = document.querySelector('.blackout');
-        const arrow = document.querySelector('.arrow-down');
+    const blackout = document.querySelector('.blackout');
+    const arrow = document.querySelector('.arrow-down');
 
-        arrow.addEventListener('click', () => {
-            blackout.style.transform = 'scale(0)';
-        });
-    }
-)();
+    arrow.addEventListener('click', () => {
+        blackout.style.transform = 'scale(0)';
+    });
+})();
 
 const init = () => {
     const myMap = new ymaps.Map("map", {
@@ -26,7 +30,7 @@ const init = () => {
 
     myMap.geoObjects
         .add(new ymaps.Placemark([60.005090, 30.245701], {
-            balloonContent: 'Здароова <strong>петух!</strong>'
+            balloonContent: 'Здарова <strong>петух!</strong>'
         }));
 };
 
